6cb70d538c2787bf903bbd8c31ab28a0d5869103
KartikKulshreshtha <76987281+KartikKulshreshtha@users.noreply.github.com>
2024-03-30 15:51:25 +0530

    Update login.js
    changes in login

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -5,22 +5,30 @@ import jwt from 'jsonwebtoken'
 
 router.post("/", async (req, res) => {
     try {
-        const {email, password} = req.body;
-        const isExist = await User.find({ email: email });
-        if(isExist.length === 0){
-            return res.status(400).json({ success: false, message: "User does not exist" })
-        }
-        else{
-            const data = {
-                userId: isExist[0]._id,
-                username: isExist[0].username,
-                email: email,
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+
+        if (!user) {
+            return res.status(400).json({ success: false, message: "User does not exist" });
+        } else {
+            // Compare passwords
+            const passwordMatch = await bcrypt.compare(password, user.password);
+
+            if (!passwordMatch) {
+                return res.status(400).json({ success: false, message: "Incorrect password" });
+            } else {
+                // Passwords match, generate token
+                const data = {
+                    userId: user._id,
+                    username: user.username,
+                    email: email,
+                }
+                const token = jwt.sign(data, process.env.SALT)
+                return res.status(200).json({ success: true, message: "Login Successfully", token: token })
             }
-            const token = jwt.sign(data, process.env.SALT)
-            return res.status(200).json({ success: true, message: "Login Successfully", token: token })
         }
     } catch (error) {
-        return res.status(400).json({ success: false, message: error.message })
+        return res.status(400).json({ success: false, message: error.message });
     }
 })
 
